78a67242122d32a032a1fe36ff2a40ab6bb69b03
rinklepoonia <rinklepoonia889@gmail.com>
2024-01-04 00:54:09 +0530

    digiency2

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const menuIcon = document.querySelector(".menuIcon")
+const menulist = document.querySelector(".menulist")
+
+menuIcon.addEventListener("click", function () {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overFlowHidden")
+})
+
+// back-to-top-================================
+const topBtn = document.querySelector(".topBtn");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
+
